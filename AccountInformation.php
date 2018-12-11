0906468449cbaf779b0448cb94e5944061f31d56
Mark Redmond <markred123@gmail.com>
2018-12-11 16:58:25 +0000

    Added nav bar to account info page

--- AccountInformation.php
+++ AccountInformation.php
@@ -31,10 +31,25 @@ session_start();
 	<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
 </head>
     <body>
+        <div id="navbar" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav navbar-right">
+                        <li><a href = "AccountInformation.php"><?php
+                            echo $_SESSION['username']; ?></a> </li>
+						<li><a href="#serv">Services</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#contact">Contact</a></li>
+						<li><a href="#home">Home</a></li>
+						<li><a href="login.php">Login</a></li>
+						<li><a href="signup.php">Sign-Up</a></li>
+						<li> <a href="SignOut.php" class="btn btn-danger">Sign Out of Your Account </a></li>
+                
+			  </ul>
+		  </div>
         <h1>Account Information</h1>
         <h2>Welcome </h2>
         <h2>Username: <?php echo $_SESSION['username']; ?> </h2>
-        <h2>Password: <?php echo $_SESSION['username']; ?> </h2>
+        <h2>Password: <?php echo $_SESSION['password']; ?> </h2>
+        <h2>You are user number<?php echo $_SESSION['id']; ?>?</h2>
         <h2>Editing coming soon TM </h2>
 
     </body>
