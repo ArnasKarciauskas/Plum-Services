99e3aeb481d7ffeac74093526f61d0748edd405f
Mark Redmond <markred123@gmail.com>
2018-12-14 00:30:42 +0000

    Working send info to database

--- JobPost.php
+++ JobPost.php
@@ -1,32 +1,15 @@
 <!DOCTYPE html>
 <html lang="eng">
     <head>
-            <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title> Login Form </title>
-
-        <!-- CSS -->
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
-		<link rel="stylesheet" href="assets/css/form-elements.css">
-        <link rel="stylesheet" href="assets/css/loginstyle.css">
-
-        <!-- Favicon and touch icons -->
-        <link rel="shortcut icon" href="assets/ico/favicon.png">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
     </head>
     <body>
         <form action="DatabaseInput.php" method="post">
-            Value1: <input type="text" name ="JobName"/>
-            Value2: <input type="text" name ="ShortDesc"/>
-            Value3: <input type="text" name ="LongDesc"/>
-            Value4: <input type="text" name ="Phone"/>
-            Value5: <input type="text" name ="Email"/>
+            Job Name: <input type="text" name ="JobName"/>
+            Short Description: <input type="text" name ="ShortDesc"/>
+            Long Description: <input type="text" name ="LongDesc"/>
+            Phone Number: <input type="text" name ="Phone"/>
+            Email Address: <input type="text" name ="Email"/>
+            Job Category <input type="text" name = "Category"/>
             <input type="Submit"/>
             
         </form>
